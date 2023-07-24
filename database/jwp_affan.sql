832733506d2a1e57604916e6abc76102c59466bc
Affan Haidar <affanhaidar232@gmail.com>
2023-07-24 23:36:17 +0700

    new database

--- database/jwp_affan.sql
+++ database/jwp_affan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Jun 2023 pada 13.37
+-- Waktu pembuatan: 24 Jul 2023 pada 18.35
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -38,12 +38,7 @@ CREATE TABLE `admin_login` (
 --
 
 INSERT INTO `admin_login` (`id`, `username`, `password`) VALUES
-(6, 'qwerty', '$2y$10$iTa4cLwURDDiXcifoG8c4usjN3uEkanqUi/K1ezoE7Se7V.MnMGzK'),
-(7, 'affanpc', '$2y$10$cq5y9qWRhmjBpr50hAdrhuC8y8AdvmU9Pz.abHlqXUKP/kqp8alFC'),
-(8, 'Affanhp', '$2y$10$OIkSR4xyRvWKFStQzoozsuX.EdU.32mlDmwAVSWWBjaOAjfBEmfo2'),
-(9, 'afwafaw', '$2y$10$/T6E8sPDcf9WR4osPkc6N.4nYJgX64/XcfJoOjMxZmOo/a7i286Yy'),
-(10, 'test1', '$2y$10$WpZyiJ5j88RUEoQra4rIou51es/ZNyy0u/vC4CryhUDnFG6lV8G6.'),
-(11, 'Affantest', '$2y$10$bwPugtLEMzaGq7PIuVoppuDRTKjmu0pbLrfRZbVWEXl16Msl//G1C');
+(6, 'qwerty', '$2y$10$iTa4cLwURDDiXcifoG8c4usjN3uEkanqUi/K1ezoE7Se7V.MnMGzK');
 
 -- --------------------------------------------------------
 
@@ -55,19 +50,20 @@ CREATE TABLE `artikel` (
   `id_artikel` int(10) NOT NULL,
   `nama_artikel` varchar(100) NOT NULL,
   `gambar_artikel` varchar(255) NOT NULL,
-  `isi_artikel` text NOT NULL
+  `isi_artikel` text NOT NULL,
+  `komentar_aktif` int(11) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `artikel`
 --
 
-INSERT INTO `artikel` (`id_artikel`, `nama_artikel`, `gambar_artikel`, `isi_artikel`) VALUES
-(71, '150-Year-Old Butter ', '175-1.jpg', '\"It really doesn\'t have much of a smell anymore,\" claims curator Bill Cantine about the block of butter recovered by archaeologists from the wreck of the steamboat Bertrand, which sank in 1865.'),
-(72, 'The Bertrand Sank in 1865 in the Missouri River', '869-2.jpg', 'The steamboat Bertrand was carrying passengers and cargo up the Missouri River to the mining towns in Montana Territory when it hit a snag in the river 30 miles north of Omaha and sank on April 1, 1865. '),
-(73, 'The Forgotten Ship Was Found Buried in a Corn Field', '200-3.jpeg', 'Two Omaha men, Jesse Pursell and Sam Corbino, began a search in 1967 that led to the discovery of the Steamboat Bertrand. The Missouri River had changed course over time, leaving the forgotten wreck in the middle of a Nebraska cornfield. Operating under a Federal contract, the pair successfully completed the excavation of the boat and its cargo in 1969. Much of the material is on display in the visitor center of DeSoto National Wildlife Refuge maintained by the U.S. Fish and Wildlife Service.'),
-(74, 'Even fabrics were recovered fairly intact from Bertrand', '671-5.jpg', 'Sixteen ladies\' capes were recovered from Bertrand, including triangular knitted capes of pinkish violet with tasseled ties at the neck, very typical of the time. '),
-(75, 'Hundreds of bottles with an astonishing range of contents', '346-6.jpeg', 'The Bertrand\'s cargo provides a extraordinary time-capsule from the Civil War era, with Underwood\'s \"Tomatoe Katsup,\" brandied peaches from Boston, fruit jellies from Baltimore, pickles from Delaware, pepper sauce from St. Louis, and clover honey from Philadelphia.');
+INSERT INTO `artikel` (`id_artikel`, `nama_artikel`, `gambar_artikel`, `isi_artikel`, `komentar_aktif`) VALUES
+(71, '150-Year-Old Butter ', '175-1.jpg', '\"It really doesn\'t have much of a smell anymore,\" claims curator Bill Cantine about the block of butter recovered by archaeologists from the wreck of the steamboat Bertrand, which sank in 1865.', 0),
+(72, 'The Bertrand Sank in 1865 in the Missouri River', '869-2.jpg', 'The steamboat Bertrand was carrying passengers and cargo up the Missouri River to the mining towns in Montana Territory when it hit a snag in the river 30 miles north of Omaha and sank on April 1, 1865. ', 1),
+(73, 'The Forgotten Ship Was Found Buried in a Corn Field', '200-3.jpeg', 'Two Omaha men, Jesse Pursell and Sam Corbino, began a search in 1967 that led to the discovery of the Steamboat Bertrand. The Missouri River had changed course over time, leaving the forgotten wreck in the middle of a Nebraska cornfield. Operating under a Federal contract, the pair successfully completed the excavation of the boat and its cargo in 1969. Much of the material is on display in the visitor center of DeSoto National Wildlife Refuge maintained by the U.S. Fish and Wildlife Service.', 0),
+(74, 'Even fabrics were recovered fairly intact from Bertrand', '671-5.jpg', 'Sixteen ladies\' capes were recovered from Bertrand, including triangular knitted capes of pinkish violet with tasseled ties at the neck, very typical of the time. ', 0),
+(75, 'Hundreds of bottles with an astonishing range of contents', '346-6.jpeg', 'The Bertrand\'s cargo provides a extraordinary time-capsule from the Civil War era, with Underwood\'s \"Tomatoe Katsup,\" brandied peaches from Boston, fruit jellies from Baltimore, pickles from Delaware, pepper sauce from St. Louis, and clover honey from Philadelphia.', 0);
 
 -- --------------------------------------------------------
 
@@ -80,32 +76,30 @@ CREATE TABLE `komentar` (
   `id_artikel` int(10) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `komentar` text NOT NULL
+  `komentar` text NOT NULL,
+  `tanggal_komentar` datetime NOT NULL,
+  `status_hide` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `komentar`
 --
 
-INSERT INTO `komentar` (`id_komentar`, `id_artikel`, `nama`, `email`, `komentar`) VALUES
-(45, 73, 'Maida Chan', 'receiver2080@protonmail.com', ' Haskell is a standardized, general-purpose purely functional programming language, with non-strict semantics and strong static typing.\",\r\n'),
-(46, 73, 'Sol Wilkinson', 'elite2024@duck.com', 'They are written as strings of consecutive alphanumeric characters, the first character being lowercase.'),
-(47, 73, 'Hilario Wilkins', 'about1897@duck.com', '    Tuples are containers for a fixed number of Erlang data types.\r\n'),
-(48, 73, 'Wendie Reeves', 'actress1872@live.com', '    I don\'t even care.\r\n'),
-(49, 73, 'Dane Mcclain', 'pencil1901@yahoo.com', '    Ports are created with\' the built-in function open_port.\r\n'),
-(50, 71, 'test', 'test', 'test'),
-(51, 71, 'Guadalupe Farrell', 'zen1992@duck.com', '    Atoms are used within a program to denote distinguished values.\r\n'),
-(52, 71, 'Darby Rollins', 'economy1859@protonmail.com', '    The sequential subset of Erlang supports eager evaluation, single assignment, and dynamic typing.\r\n'),
-(53, 71, 'Lawrence Bradford', 'director1858@protonmail.com', '    The Galactic Empire is nearing completion of the Death Star, a space station with the power to destroy entire planets.\r\n'),
-(54, 72, 'test', 'test', 'test'),
-(55, 72, 'Burt Walter', 'arguments1891@duck.com', '    He looked inquisitively at his keyboard and wrote another sentence.\r\n'),
-(56, 72, 'Marketta Mcdowell', 'mystery1887@example.org', '    I don\'t even care.\r\n'),
-(57, 74, 'test', 'test', 'test'),
-(58, 74, 'Bruno Wallace', 'lunch1957@yandex.com', '    Where are my pants?\r\n'),
-(59, 74, 'Ward Tran', 'likes1896@duck.com', '    Atoms are used within a program to denote distinguished values.\r\n'),
-(60, 75, 'test', 'test', 'test'),
-(61, 75, 'Grisel Strong', 'origin2012@outlook.com', '    The sequential subset of Erlang supports eager evaluation, single assignment, and dynamic typing.\r\n'),
-(62, 72, 'afaw', 'awfaw', '');
+INSERT INTO `komentar` (`id_komentar`, `id_artikel`, `nama`, `email`, `komentar`, `tanggal_komentar`, `status_hide`) VALUES
+(94, 74, 'Sienna ', 'Sienna@gmail.com', 'Faucibus justo mi dignissim dictum integer rutrum aliquet suscipit. Scelerisque tincidunt taciti orci facilisi accumsan natoque Sit, ipsum gravida lorem egestas dapibus velit enim elementum nullam et, litora torquent dignissim massa quis pellentesque. Aliquam, mattis, quam blandit posuere aliquam mollis tortor potenti inceptos lacus tellus sapien cum. Sit volutpat purus congue molestie consequat leo. Ligula vel lacus feugiat inceptos nulla.', '2023-07-24 19:19:40', 0),
+(95, 74, 'Sienna ', 'Sienna@gmail.com', 'Faucibus justo mi dignissim dictum integer rutrum aliquet suscipit. Scelerisque tincidunt taciti orci facilisi accumsan natoque Sit, ipsum gravida lorem egestas dapibus velit enim elementum nullam et, litora torquent dignissim massa quis pellentesque. Aliquam, mattis, quam blandit posuere aliquam mollis tortor potenti inceptos lacus tellus sapien cum. Sit volutpat purus congue molestie consequat leo. Ligula vel lacus feugiat inceptos nulla.', '2023-07-24 19:19:50', 0),
+(96, 71, 'Test', 'Test', 'Test', '2023-07-24 19:21:08', 0),
+(97, 72, 'Test', 'Test', 'Test', '2023-07-24 19:21:14', 0),
+(98, 73, 'Test', 'Test', 'Test', '2023-07-24 19:21:19', 0),
+(99, 75, 'Test', 'Test', 'Test', '2023-07-24 19:21:28', 0),
+(100, 72, 'Eaton Marks', 'tellus.eu.augue@yahoo.org', 'odio tristique pharetra. Quisque ac libero nec ligula consectetuer rhoncus. Nullam velit dui, semper et, lacinia vitae, sodales at, velit. Pellentesque ultricies dignissim lacus. Aliquam rutrum lorem ac risus. Morbi metus. Vivamus euismod urna. Nullam lobortis quam a felis ullamcorper viverra. Maecenas iaculis aliquet diam. Sed diam lorem, auctor\"\r\n', '2023-07-24 19:27:00', 0),
+(101, 72, 'Sonia Massey', 'tristique.senectus@icloud.com', 'dui quis accumsan convallis, ante lectus convallis est, vitae sodales nisi magna sed dui. Fusce aliquam, enim nec tempus scelerisque, lorem ipsum sodales purus, in molestie tortor nibh sit amet orci. Ut sagittis lobortis mauris. Suspendisse aliquet molestie tellus. Aenean egestas hendrerit neque. In ornare sagittis felis. Donec tempor,\"\r\n', '2023-07-24 19:27:31', 0),
+(103, 71, 'Akeem Morales', 'tellus.eu@outlook.com', 'risus. Duis a mi fringilla mi lacinia mattis. Integer eu lacus. Quisque imperdiet, erat nonummy ultricies ornare, elit elit fermentum risus, at fringilla purus mauris a nunc. In at pede. Cras vulputate velit eu sem. Pellentesque ut ipsum ac mi eleifend egestas. Sed pharetra, felis eget varius ultrices, mauris\"\r\n', '2023-07-24 19:28:01', 0),
+(104, 71, 'Ursa Church', 'ornare.egestas@protonmail.net', 'vulputate dui, nec tempus mauris erat eget ipsum. Suspendisse sagittis. Nullam vitae diam. Proin dolor. Nulla semper tellus id nunc interdum feugiat. Sed nec metus facilisis lorem tristique aliquet. Phasellus fermentum convallis ligula. Donec luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget, venenatis a, magna. Lorem ipsum\"\r\n', '2023-07-24 19:28:14', 0),
+(105, 73, 'Portia Diaz', 'sed.congue@protonmail.com', 'semper pretium neque. Morbi quis urna. Nunc quis arcu vel quam dignissim pharetra. Nam ac nulla. In tincidunt congue turpis. In condimentum. Donec at arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae Donec tincidunt. Donec vitae erat vel pede blandit congue. In scelerisque\"\r\n', '2023-07-24 19:28:30', 0),
+(106, 75, 'Stephen Dotson', 'in.hendrerit.consectetuer@google.ca', 'eu dolor egestas rhoncus. Proin nisl sem, consequat nec, mollis vitae, posuere at, velit. Cras lorem lorem, luctus ut, pellentesque eget, dictum placerat, augue. Sed molestie. Sed id risus quis diam luctus lobortis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Mauris ut quam\"\r\n', '2023-07-24 19:28:59', 0),
+(107, 71, 'test hide 1', 'test hide 1', 'test hide 1 test hide 1test hide 1 test hide 1', '2023-07-24 22:10:05', 0),
+(108, 72, 'Test komentar 3', 'ianwfia', 'Warning: Undefined variable $result_comments in C:xampphtdocsLSPPagesUserarticle.php on line 103\r\n\r\nFatal error: Uncaught TypeError: mysqli_num_rows(): Argument #1 ($result) must be of type mysqli_result, null given in C:xampphtdocsLSPPagesUserarticle.php:103 Stack trace: #0 C:xampphtdocsLSPPagesUserarticle.php(103): mysqli_num_rows(NULL) #1 {main} thrown in C:xampphtdocsLSPPagesUserarticle.php on line 103', '2023-07-24 23:24:31', 0);
 
 --
 -- Indexes for dumped tables
@@ -150,7 +144,7 @@ ALTER TABLE `artikel`
 -- AUTO_INCREMENT untuk tabel `komentar`
 --
 ALTER TABLE `komentar`
-  MODIFY `id_komentar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id_komentar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
